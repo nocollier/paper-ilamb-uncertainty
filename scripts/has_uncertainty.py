2dcb72b2d8e3d37e638806f2eb87b6ee949e7e3f
Nathan Collier <nathaniel.collier@gmail.com>
2026-08-14 08:34:37 -0400

    add check for unc

---
+++ scripts/has_uncertainty.py
@@ -0,0 +1,42 @@
+from pathlib import Path
+
+import pandas as pd
+import xarray as xr
+
+
+def check_dataset(nc_file: Path) -> list[dict[str, str | Path]]:
+    """Return the dataset, variable, and uncertainty variable if present in the file."""
+    ds = xr.open_dataset(nc_file)
+    out = []
+    for var, da in ds.items():
+        uncert = [
+            attr
+            for attr in [
+                da.attrs.get(attr, None) for attr in ["bounds", "ancillary_variables"]
+            ]
+            if attr is not None and attr in ds
+        ]
+        if uncert:
+            for un in uncert:
+                out.append({"dataset": nc_file, "variable": var, "uncertainty": un})
+    return out
+
+
+def has_uncertainty(root: str | Path) -> pd.DataFrame:
+    """Recursively check dataset under the given root for the presence of uncertainty."""
+    root = Path(root)
+    df = []
+    for sdir, _, files in root.walk():
+        for f in files:
+            if not f.endswith(".nc"):
+                continue
+            df += check_dataset(sdir / f)
+    df = pd.DataFrame(df)
+    for r, row in df.iterrows():
+        df.loc[r, "dataset"] = row["dataset"].relative_to(root)  # type: ignore
+    df.sort_values("dataset").reset_index(inplace=True)
+    return df
+
+
+if __name__ == "__main__":
+    df = has_uncertainty("/home/nate/.cache/ilamb3/0.1")
