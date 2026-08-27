ce32dfcf5f12009ad98d73d4fdaa8be4fae1bca7
Nathan Collier <nathaniel.collier@gmail.com>
2026-08-27 13:28:12 -0400

    update run options

--- ilamb/fetch.sh
+++ ilamb/fetch.sh
@@ -1,2 +1,9 @@
+MODELS=("BCC-ESM1" "CanESM5" "CESM2" "E3SM-1-1" "EC-Earth3-Veg" "GFDL-ESM4" "GISS-E2-1-G" "IPSL-CM6A-LR" "MIROC-ES2L" "MPI-ESM1-2-LR" "UKESM1-0-LL")
+mkdir -p _models
+for model in "${MODELS[@]}"
+do
+  echo "Querying ESGF for $model..."
+  ilamb esgf study_part.yaml --source-id $model
+done
+mv *.csv _models
 ilamb fetch study.yaml
-ilamb esgf study.yaml --source-id CanESM5
--- ilamb/run.sh
+++ ilamb/run.sh
@@ -1,3 +1,38 @@
+#!/bin/bash -l
+#SBATCH --job-name=ilamb3
+#SBATCH --account=cli137
+#SBATCH --time=3:00:00
+#SBATCH --nodes=4
+#SBATCH --output=%x-%j.log
 
-python ../scripts/expand_config.py study_part.yaml > study.yaml
-ilamb run study.yaml --model-db CanESM5.csv
+# Setup: This makes sure that we have pointers to where the reference
+# data is stored as well as the root of the model data. Finally we
+# make sure the environment is activated and ready to go.
+export ILAMB_ROOT=/lustre/orion/cli137/world-shared/ilamb3-data
+export ESGF_ROOT=/lustre/orion/cli137/world-shared/ESGF-data
+cd $SLURM_SUBMIT_DIR
+source $SLURM_SUBMIT_DIR/../.venv/bin/activate
+
+# Mysterious incantations: Some functions in xarray are not threadsafe
+# (like sel) and cause strange problems. h/t Min Xu
+export HDF5_USE_FILE_LOCKING=FALSE
+export NETCDF4_DISABLE_PTHREADS=1
+export OMP_NUM_THREADS=1
+
+# Uncertainty run
+srun -n 16 --cpu-bind=cores --distribution=cyclic python -m mpi4py.futures \
+$SLURM_SUBMIT_DIR/../.venv/bin/ilamb run \
+study.yaml \
+--model-db _models/BCC-ESM1.csv \
+--model-db _models/CanESM5.csv \
+--model-db _models/CESM2.csv \
+--model-db _models/E3SM-1-1.csv \
+--model-db _models/EC-Earth3-Veg.csv \
+--model-db _models/GFDL-ESM4.csv \
+--model-db _models/GISS-E2-1-G.csv \
+--model-db _models/IPSL-CM6A-LR.csv \
+--model-db _models/MIROC-ES2L.csv \
+--model-db _models/MPI-ESM1-2-LR.csv \
+--model-db _models/UKESM1-0-LL.csv \
+--output-path _build/Uncertainty 
+
